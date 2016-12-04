a97acadcc50b6fbe867d1a9829e2d9c5681a977d
Aravind9949 <aravind.m14@iiits.in>
2016-12-04 23:15:10 +0530

    Add files via upload

---
+++ MAin.java
@@ -0,0 +1,13 @@
+	
+public class MAin {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubew
+		loginDemo frame =new loginDemo();
+	      frame.main(null);
+
+	} 
+}
